d97dd6266f18baa36660fb74526a88634d6cfc5b
umer <umeraftab763@gmail.com>
2025-03-17 17:29:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port =  8080;
 
 // Set __dirname and __filename
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // PostgreSQL Connection
-/*const pool = new pkg.Pool({
-  connectionString: process.env.DATABASE_URL,
+const pool = new pkg.Pool({
+  connectionString: 'postgresql://neondb_owner:npg_sw58OFiXJGeC@ep-odd-truth-a5etezja-pooler.us-east-2.aws.neon.tech/neondb?sslmode=requires',
 });
 
 pool.connect((err) => {
@@ -25,7 +25,7 @@ pool.connect((err) => {
   }
   console.log("Connected to PostgreSQL");
 });
-*/
+
 // Middleware to serve static HTML
 app.use(express.static("."));
 
@@ -34,21 +34,17 @@ app.get("/", (req, res) => {
 });
 
 // API Route to Fetch Data
-/*app.get("/data", (req, res) => {
-  pool.query("INSERT INTO public.a (column1) VALUES ('hey');", (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    pool.query("SELECT * FROM public.a;", (err, results) => {
+app.get("/data", (req, res) => {
+  
+    pool.query("SELECT * FROM public.b;", (err, results) => {
       if (err) {
         res.status(500).json({ error: err.message });
         return;
       }
       res.json(results.rows);
-    });
+    
   });
-});*/
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
